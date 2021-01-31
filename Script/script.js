08777b1f69d04d9d473b711068cba794f7e84415
godwinchigozie679 <68194055+godwinchigozie679@users.noreply.github.com>
2021-01-31 18:15:24 +0100

    First commit

---
+++ Script/script.js
@@ -0,0 +1,16 @@
+
+// //Showcase Load
+// var display = ["Crypto Bootcamp", "The Number One Free Blockchain and Cryptocurrency Educational Institute in Africa"];
+
+// // let startText = 0;
+// // let currentText = display[startText];
+// let startText = 0;
+// const writeShowase = document.getElementById("writeShowcase");
+// function showCase() {    
+//     let currentText = display[startText];    
+//     writeShowcase.innerHTML = display[startText];
+
+// }
+// showCase();
+
+
